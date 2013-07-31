f6da4f9d0774bfd883ce3bd231fdf944b1b7923f
Patrick McMorran <kingrolloinc@sbcglobal.net>
2013-07-31 07:50:13 -0400

    First Commit of project

---
+++ MultidropBus/MDBPort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace MultidropBus
+{
+    public class MDBPort
+    {
+        SerialPort Port;
+
+        //usemyserialPort.Parity = Parity.Mark; myserialPort.Parity = Parity.Space; for 9th bit.
+
+
+        public MDBPort(string COM)
+        {
+            this.Port = new SerialPort(COM, 9600); //MDB Protocol always uses a 9600 Baud Rate
+        }
+
+    }
+}
