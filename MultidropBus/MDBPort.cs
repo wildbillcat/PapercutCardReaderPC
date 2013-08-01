5eb8f9721e4b4bd7072d0f3037057c449bb28d2c
Patrick McMorran <kingrolloinc@sbcglobal.net>
2013-08-01 08:14:59 -0400

    updated MDB Port, considered porting to C++

--- MultidropBus/MDBPort.cs
+++ MultidropBus/MDBPort.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
+using System.Collections;
 
 namespace MultidropBus
 {
@@ -13,11 +14,17 @@ namespace MultidropBus
 
         //usemyserialPort.Parity = Parity.Mark; myserialPort.Parity = Parity.Space; for 9th bit.
 
+        BitArray inputBuffer;
 
         public MDBPort(string COM)
         {
             this.Port = new SerialPort(COM, 9600); //MDB Protocol always uses a 9600 Baud Rate
         }
+
+        public BitArray ReadByte()
+        {
+            BitArray inputByte = new BitArray(9);
+            
 
     }
 }
